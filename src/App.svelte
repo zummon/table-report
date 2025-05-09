7c725a2396cd249de87d5661e0168b011078d9dd
zummon <yoyotee235@gmail.com>
2025-05-09 18:04:52 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+	let data = $state([['head','desc','amount'],['type1','value',120],['type2','value',100],['type1','',80]])
+	let acts = $state([])
+	let rawActs = ['','group','total']
+	let report = $derived.by(() => {
+		let body = [[]]
+		let footers = []
+		data.slice(1).forEach((row, rowindex) => {
+			row.forEach((col, colindex) => {
+				acts[colindex] == 'group'
+				if (!body[rowindex]) {
+					body[rowindex] = []
+				}
+				body[rowindex][colindex] = col
+				if (acts[colindex] == 'total') {
+					if (!footers[colindex]) {
+						footers[colindex] = 0
+					}
+					footers[colindex] += +col
+				}
+			})
+		})
+		return [data[0], ...body, footers]
+	});
+</script>
+
+Column <input type="number">
+Subtotal <input type="checkbox">
+
+<table>
+	<thead>
+		<tr>
+			{#each data[0] as col, colindex}
+				<td contenteditable="true" onchange={() => {}}>
+					{col}
+					<select value={acts[colindex]} onchange={(e) => {
+						acts[colindex] = e.target.value
+					}}>
+						{#each rawActs as value}
+							<option value={value}>{value}</option>
+						{/each}
+					</select>
+				</td>
+			{/each}
+		</tr>
+	</thead>
+	<tbody>
+	{#each report.slice(1) as row, rowindex}
+		<tr>
+			{#each row as col, colindex}
+				<td contenteditable="true" onchange={() => {
+	
+				}}>{col}</td>
+			{/each}
+		</tr>
+	{/each}
+	</tbody>
+</table>

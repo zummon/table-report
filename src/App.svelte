830aae7ba841175cb68c6b941891d22ed2a2605f
zummon <zummon.space@gmail.com>
2025-05-10 19:50:05 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,54 @@
 <script>
-	let rawActs = ['','group','total']
+	import { onMount } from "svelte";
 
+	let user = $state({ t: [], })
 	let data = $state([['head','desc','amount'],['type1','value',120],['type2','value',100],['type1','',80]])
-	let acts = $state([])
 
-	let report = $derived.by(() => {
+	let reports = $derived.by(() => {
 		let body = [[]]
 		let footers = []
 		data.slice(1).forEach((row, rowindex) => {
 			row.forEach((col, colindex) => {
-				acts[colindex] == 'group'
-				if (!body[rowindex]) {
-					body[rowindex] = []
-				}
-				body[rowindex][colindex] = col
-				if (acts[colindex] == 'total') {
-					if (!footers[colindex]) {
-						footers[colindex] = 0
-					}
-					footers[colindex] += +col
-				}
+
 			})
 		})
-		return [data[0], ...body, footers]
+		return []
 	});
+
+	function arrize(rows) {
+		let result = [[]]
+		rows.split('\n').forEach((row, rowindex) => {
+			result[rowindex] = []
+			row.split('	').forEach((col, colindex) => {
+				result[rowindex][colindex] = col
+			})
+		})
+		return result
+	}
+
+	onMount(() => {
+		let params = new URLSearchParams()
+		for (let key in user) {
+			user[key] = params.getAll(key)
+		}
+	})
 </script>
 
-Column <input type="number">
-Subtotal <input type="checkbox">
+<textarea class="" onchange={(e)=>{
+	let value = e.currentTarget.value
+	data = arrize(value)
+}}></textarea>
 
-<table>
-	<thead>
-		<tr>
-			{#each data[0] as col, colindex}
-				<td onchange={() => {}}>
-					{col}
-					<select value={acts[colindex]} onchange={(e) => {
-						acts[colindex] = e.currentTarget.value
-					}}>
-						{#each rawActs as value}
-							<option value={value}>{value}</option>
-						{/each}
-					</select>
-				</td>
+{#each reports as report}
+	<table class="">
+		<tbody class="">
+			{#each report as row, rowindex}
+				<tr>
+					{#each row as col, colindex}
+						<td>{col}</td>
+					{/each}
+				</tr>
 			{/each}
-		</tr>
-	</thead>
-	<tbody>
-	{#each report.slice(1) as row, rowindex}
-		<tr>
-			{#each row as col, colindex}
-				<td onchange={() => {
-	
-				}}>{col}</td>
-			{/each}
-		</tr>
-	{/each}
-	</tbody>
-</table>
+		</tbody>
+	</table>
+{/each}
